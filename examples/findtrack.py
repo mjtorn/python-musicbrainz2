dfb0b7f59a243e11aaafa8df451ff0bb7a810692
matt <matt@b0b80210-5d09-0410-99dd-b4bd03f891c0>
2006-03-07 16:52:03 +0000

    Added an example which searches for a track.
    git-svn-id: http://svn.musicbrainz.org/python-musicbrainz2/trunk@152 b0b80210-5d09-0410-99dd-b4bd03f891c0

---
+++ examples/findtrack.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+import sys
+import logging
+from musicbrainz2.webservice import Query, TrackFilter, WebServiceError
+
+logging.basicConfig(
+        level=logging.DEBUG,
+	format='%(levelname)-8s: %(message)s'
+)
+
+
+if len(sys.argv) < 2:
+	print "Usage: findartist.py 'track name' ['artist name']"
+	sys.exit(1)
+
+if len(sys.argv) > 2:
+	artistName = sys.argv[2]
+else:
+	artistName = None
+
+q = Query()
+
+try:
+	f = TrackFilter(title=sys.argv[1], artistName=artistName)
+	tracks = q.getTracks(f)
+except WebServiceError, e:
+	print 'Error:', e
+	sys.exit(1)
+
+
+for track in tracks:
+	print "Id        :", track.getId()
+	print "Name      :", track.getTitle()
+	print "Artist    :", track.getArtist().getUniqueName()
+	print
+
